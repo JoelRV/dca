6a38ec6bfb872eaf4d607e9cad32b65e88211c8e
JoelRV <joelrrv@hotmail.com>
2019-05-22 16:49:25 +0200

    trying to define model_fn and data_fn inside python scripts as shown in kopt doc

---
+++ dca/data.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 22 16:42:28 2019
+
+@author: joelrv
+"""
+
+
+def data_fn(norm_input_log, norm_input_zeromean, norm_input_sf):
+
+    ad = adata.copy()
+    ad = io.normalize(ad,
+                      size_factors=norm_input_sf,
+                      logtrans_input=norm_input_log,
+                      normalize_input=norm_input_zeromean)
+
+
+    x_train = {'count': ad.X, 'size_factors': ad.obs.size_factors}
+    #x_train = ad.X
+    y_train = ad.raw.X
+    del ad
+    gc.collect()
+    return (x_train, y_train),
