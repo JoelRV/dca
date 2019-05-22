7702d23ad638a76608960c9d917b069f8d7e28e8
JoelRV <joelrrv@hotmail.com>
2019-05-22 15:12:14 +0200

    tried applying adata normalization outside of data_fn

--- dca/hyper.py
+++ dca/hyper.py
@@ -51,6 +51,11 @@ def hyper(args):
     adata = io.read_dataset(args.input,
                             transpose=args.transpose,
                             test_split=False)
+                            
+    adata = io.normalize(adata,
+                         size_factors=args.sizefactors,
+                         logtrans_input=args.loginput,
+                         normalize_input=args.norminput)
 
     hyper_params = {
             "data": {
@@ -78,16 +83,16 @@ def hyper(args):
     }
 
     def data_fn(norm_input_log, norm_input_zeromean, norm_input_sf):
+#
+#        ad = adata.copy()
+#        ad = io.normalize(ad,
+#                          size_factors=norm_input_sf,
+#                          logtrans_input=norm_input_log,
+#                          normalize_input=norm_input_zeromean)
 
-        ad = adata.copy()
-        ad = io.normalize(ad,
-                          size_factors=norm_input_sf,
-                          logtrans_input=norm_input_log,
-                          normalize_input=norm_input_zeromean)
-
-        x_train = {'count': ad.X, 'size_factors': ad.obs.size_factors}
+        x_train = {'count': adata.X, 'size_factors': adata.obs.size_factors}
         #x_train = ad.X
-        y_train = ad.raw.X
+        y_train = adata.raw.X
 
         return (x_train, y_train),
 
