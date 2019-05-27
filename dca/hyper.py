45775768e5e7d1dcc346291506845de142efafcf
JoelRV <joelrrv@hotmail.com>
2019-05-27 11:09:40 +0200

    removed print statements in data_fn

--- dca/hyper.py
+++ dca/hyper.py
@@ -114,10 +114,10 @@ def hyper(args):
                      normalize_input=norm_input_zeromean)
                      
         x_train = {'count': ad.X, 'size_factors': ad.obs.size_factors}
-        print(x_train)
+#        print(x_train)
         #x_train = ad.X
         y_train = adata.X
-        print(y_train)
+#        print(y_train)
         gc.collect()
         return (x_train, y_train),
 
